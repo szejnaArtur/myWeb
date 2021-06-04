b97fa3738480a0e85b8786db64e71f785ce4c197
szejnaArtur <50076342+szejnaArtur@users.noreply.github.com>
2021-06-04 13:06:52 +0200

    add animation

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -5,11 +5,28 @@ $(document).ready(function () {
         } else {
             $('.navbar').removeClass("sticky");
         }
+
+        if (this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    $('.scroll-up-btn').click(function (){
+        $('html').animate({scrollTop: 0});
     });
 
     $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
+    });
+
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Programer", "Manager", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 80,
+        loop: true
     });
 
     $('.carousel').owlCarousel({
